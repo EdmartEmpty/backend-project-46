f3d0eee530d7680c0d086db5e4c4e48f09a41fce
Эдуард <edmart666@gmail.com>
2026-07-05 19:05:34 +0300

    add number in normalizeValue

--- src/helpers/normalizeValue.js
+++ src/helpers/normalizeValue.js
@@ -1,5 +1,5 @@
 const normalizeValue = (element) => {
-  const types = { string: `'${element}'`, object: `[complex value]`, boolean: `${element}` }
+  const types = { string: `'${element}'`, object: `[complex value]`, boolean: `${element}`, number: `${element}` }
   if (element === null) {
     return null
   }
