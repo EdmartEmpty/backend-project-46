abd4862e09f1d23a9e70f8637e654b4ade8d331f
Эдуард <edmart666@gmail.com>
2026-07-25 21:12:13 +0300

    update buildDifference, add switch consrturcion inside

--- src/helpers/buildDifference.js
+++ src/helpers/buildDifference.js
@@ -6,26 +6,20 @@ const buildDifference = (file1, file2) => {
   const keys = _.union(keys1, keys2)
 
   const allKeys = _.sortBy(keys)
-  const result = allKeys.map((key) => {
-    if (!Object.hasOwn(file1, key)) {
-      return { key, type: 'added', value: file2[key] }
-    }
-    else if (!Object.hasOwn(file2, key)) {
-      return { key, type: 'deleted', value: file1[key] }
-    }
-    else if (file1[key] !== file2[key]) {
-      if (_.isObject(file1[key]) && _.isObject(file2[key])) {
+  return allKeys.map((key) => {
+    switch (true) {
+      case !Object.hasOwn(file1, key):
+        return { key, type: 'added', value: file2[key] }
+      case !Object.hasOwn(file2, key):
+        return { key, type: 'deleted', value: file1[key] }
+      case _.isObject(file1[key]) && _.isObject(file2[key]):
         return { key, type: 'nested', children: buildDifference(file1[key], file2[key]) }
-      }
-      else {
+      case file1[key] !== file2[key]:
         return { key, type: 'changed', oldValue: file1[key], newValue: file2[key] }
-      }
-    }
-    else {
-      return { key, type: 'unchanged', value: file1[key] }
+      default:
+        return { key, type: 'unchanged', value: file1[key] }
     }
   })
-  return result
 }
 
 export default buildDifference
